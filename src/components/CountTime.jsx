52232471a1e3b76003cc69e81820def2d31e707a
a41220 <ducthang642002@gmail.com>
2023-05-26 09:05:56 +0700

    first commit

---
+++ src/components/CountTime.jsx
@@ -0,0 +1,56 @@
+import "./CountTime.scss"
+import { useRef, useState } from "react";
+
+const CountTime = () => {
+    const [timer, setTimer] = useState(0)
+    const countRef = useRef(null)
+
+    const handleStart = () => {
+        countRef.current = setInterval(() => {
+            setTimer((timer) => timer + 1)
+        }, 1000)
+        
+    }
+
+    const handlePause = () => {
+        clearInterval(countRef.current)
+    }
+
+    const handleReset = () => {
+        clearInterval(countRef.current)
+        setTimer(0)
+    }
+
+    const formatTime = () => {
+        const getSeconds = `0${(timer % 60)}`.slice(-2)
+        const minutes = `${Math.floor(timer / 60)}`
+        const getMinutes = `0${minutes % 60}`.slice(-2)
+        const getHours = `0${Math.floor(timer / 3600)}`.slice(-2)
+    
+        return `${getHours} : ${getMinutes} : ${getSeconds}`
+      }
+    
+    
+
+    return (
+        <div className="countTime-container">
+            <div className="activity">
+                <div className="btn start" onClick={handleStart}>
+                    Start
+                </div>
+                <div className="btn pause" onClick={handlePause}>
+                    Pause
+                </div>
+                <div className="btn reset" onClick={handleReset}>
+                    Reset
+                </div>
+            </div>
+
+            <div className="clock">
+                <span>{formatTime()}</span>
+            </div>
+        </div>
+    );
+}
+
+export default CountTime;
